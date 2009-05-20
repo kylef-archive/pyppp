13ca3eaa2f6a0ae8aedc86b6c5465831b393a4d9
Kyle Fuller <inbox@kylefuller.co.uk>
2009-05-20 14:36:35 +0100

    Moved documentation to Sphinx/docutils

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+import os, sys, pyppp
+
+source_suffix = '.txt'
+master_doc = 'contents'
+project = 'PyPPP'
+copyright = 'Kyle Fuller'
+version = pyppp.__version__
+release = version
+today_fmt = '%B %d, %Y'
+add_function_parentheses = True
+add_module_names = False
